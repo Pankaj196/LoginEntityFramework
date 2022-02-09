3df81ce31b36137077fc5ca47fd3a0a4b0c43f81
pdhakad <pdhakad@AZ-002>
2022-02-09 08:42:03 +0530

    Add project files.

---
+++ EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using LoginEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LoginEntityFramework
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() : base("name=customerDbConnectionString")
+        {
+
+        }
+
+        public DbSet<Employee> employee { get; set; }
+    }
+}
---
+++ EmployeeLogin.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginEntityFramework
+{
+    public partial class CustomerLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            EmployeeContext empContext = new EmployeeContext();
+            var query = from Employee in empContext.employee where Employee.username == txtUName.Text
+                        && Employee.passowrd == txtPwd.Text
+                        select Employee;
+            
+            
+            var username = "";
+            var password = "";
+           foreach(var employee in query)
+            {
+                username = employee.username;
+                Response.Write(username);
+                password = employee.passowrd;
+                Response.Write(password);
+            }
+
+           if(query.Count() > 0)
+            {
+                Response.Redirect("LoginSuccess.aspx");
+            }
+            else
+            {
+                Response.Write("Invalid Cred");
+            }
+        }
+    }
+}
